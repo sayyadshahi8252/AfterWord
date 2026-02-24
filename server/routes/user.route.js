af4cf82f9d825612df65d3c9f7f734142ac6137d
Sayyad Shahi <sayyadshahi1805@gmail.com>
2026-02-24 13:52:29 +0530

    multer

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,10 +1,14 @@
-import express from "express"
-import {registeruser,loginuser,refreshAccessToken} from "../controllers/user.controller.js"
+import express from "express";
+import { registeruser, loginuser, refreshAccessToken } from "../controllers/user.controller.js";
 import multer from "multer";
-const router=express.Router();
-const upload = multer({ dest: "uploads/" }); 
-router.post("/register",upload.single("avatar"),registeruser)
-router.post('/login',loginuser)
-router.post('/refresh-token',refreshAccessToken)
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
+
+router.post("/register", upload.single("avatar"), registeruser);
+router.post("/login", loginuser);
+router.post("/refresh-token", refreshAccessToken);
 
 export default router;
